247737480c49aa73b234dc8221451c3f0ce02860
Zoltan Vegh <zoli0829@gmail.com>
2025-02-23 16:06:27 +0000

    abstracted the header view
    abstracted the header view so it works now with multiple views

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -16,7 +16,7 @@ struct LoginView: View {
         NavigationView{
             VStack{
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List", subtitle: "Get things done", angle: 15, background: .pink)
                 
                 // Login Form
                 Form{
@@ -40,6 +40,7 @@ struct LoginView: View {
                     }
                     .padding()
                 }
+                .offset(y: -50)
                 
                 // Create Account
                 VStack{
