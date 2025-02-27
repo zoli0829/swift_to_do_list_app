8dc580d8cb0c81c901a2d43752a5405f53b96a95
Zoltan Vegh <zoli0829@gmail.com>
2025-02-27 19:18:10 +0000

    im so tired please send help
    doctor doesnt want to prescribe sleeping pills

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+/// ViewModel for single to do list items (each row in items list)
+class ToDoListItemViewViewModel: ObservableObject {
+    init() {}
+}
--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -13,9 +13,24 @@ struct MainView: View {
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
             // signed in
-            ToDoListView()
+            accountView
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem{
+                    Label("Home", image: "house")
+                }
+            
+            ProfileView()
+                .tabItem{
+                    Label("Profile", image: "person.circle")
+                }
         }
     }
 }
