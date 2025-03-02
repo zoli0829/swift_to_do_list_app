c5b187de738ddd4ff757ef35445f1c4af830b238
Zoltan Vegh <zoli0829@gmail.com>
2025-03-02 09:54:02 +0000

    NewItemView logic
    Created the UI for NewItemView, created the ViewModel logic to verify that we can save the data

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
     init() {}
+    
+    func save() {
+        
+    }
+    
+    // computed property
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        // 86400 seconds in a day
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
 }
